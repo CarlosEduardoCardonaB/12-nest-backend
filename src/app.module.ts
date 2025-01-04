7ec9ca8f1dcfd851a31edeba6278e69a36f0bae4
Carlos Eduardo <caeca425@gmail.com>
2025-01-03 21:32:16 -0500

    se realizan configuraciones iniciales de validaciones de datos de entrada DTO data transfer object

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,  
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  // constructor(){
+  //Este nos sirve para ver que tenemos en las variables de entorno
+  //   console.log(process.env)
+  // }
+
+}
